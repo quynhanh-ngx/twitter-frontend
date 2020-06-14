c5653526c39d0630b36c4badc61eb06d36a4ab31
Anh Quynh Nguyen <quynhhanhh@Anhs-MacBook-Pro.local>
2020-06-14 14:12:19 -0400

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import {Form, TextArea} from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor="story">Tell us your story:</label>
+
+        <textarea id="story" name="story"
+                  rows="5" cols="33">
+It was a dark and stormy night...
+</textarea>
+
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
