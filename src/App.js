49c90d821bf1ddc183b7a910f7d960cd8a52f5eb
Anh Quynh Nguyen <quynhhanhh@Anhs-MacBook-Pro.local>
2020-06-14 16:46:06 -0400

    Add basic ui components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import {Form, TextArea} from 'semantic-ui-react';
+import {Button, Header} from "semantic-ui-react";
+import 'semantic-ui-css/semantic.min.css';
+import MySidebar from "./MySidebar";
+import MySearch from "./MySearch";
+import MyFeed from "./MyFeed";
+import MyTweetbox from "./MyTweetbox";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <label htmlFor="story">Tell us your story:</label>
-
-        <textarea id="story" name="story"
-                  rows="5" cols="33">
-It was a dark and stormy night...
-</textarea>
-
-
-        <p>
-          Hi.
-        </p>
-
-      </header>
+      <div className="grid-container">
+        <div className="logo">
+            <Header as='h1' color = 'blue'>Twiiter</Header>
+        </div>
+        <div className="tweetbox">
+          <MyTweetbox/>
+        </div>
+        <div className="news">Yoooooo</div>
+        <div className="search"><MySearch/></div>
+        <div className="messages"><MyFeed/></div>
+    <div className="sidebar"><MySidebar/>
+        </div>
+        <div className="submit-area">
+          <div className="submit-box">
+              <Button inverted color='blue'>
+                  Submit
+              </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
